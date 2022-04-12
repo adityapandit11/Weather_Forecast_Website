aa6b69107ffdfa59fcf7d86d431685c41588f4c7
adityapandit11 <66800459+adityapandit11@users.noreply.github.com>
2022-04-13 02:50:09 +0530

    fifth commit

--- script.js
+++ script.js
@@ -286,3 +286,16 @@ function news_down_btn5() {
   }
 
 }
+
+        document.addEventListener("keydown", (e) => {
+  // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+  // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+  // THIS WILL ONLY DISABLE CONTROL AND F12
+  if (e.ctrlKey || e.keyCode==123) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+});
+        document.addEventListener("contextmenu", (e) => {
+          e.preventDefault();
+        }, false);
